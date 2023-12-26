fabd7cd889e070f4ad0cf43d699017dddab1b1b9
Warren Fletcher <warren@fletch.band>
2023-12-26 17:49:39 +0200

    Vulkan Driver is connected to GLSW window manager

--- blackbox/blackbox/main.cpp
+++ blackbox/blackbox/main.cpp
@@ -1,14 +1,126 @@
-//
-//  main.cpp
-//  blackbox
-//
-//  Created by Warren Fletcher on 26/12/2023.
-//
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+#include <vector>
+#include <sstream>
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class BlackBoxEngine {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    struct vec3d {
+        float x,y,z;
+    };
+
+    struct triangle {
+        vec3d p[3];
+    };
+    struct mesh
+    {
+        std::vector<triangle>tris;
+    };
+    
+    
+    
+    VkInstance instance;
+    mesh meshCube;
+    GLFWwindow* window;
+    VkApplicationInfo appInfo{};
+    void initWindow() {
+        createInstance();
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        
+        std::vector<const char*> requiredExtensions;
+
+        for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        
+        
+        createInfo.enabledLayerCount = 0;
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS) {
+            std::ostringstream oss;
+            oss << "Failed to create Instance! " << result;
+            throw std::runtime_error(oss.str());
+        }
+        meshCube.tris = {
+            {0.0f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f, 0.0f},
+            {0.0f, 0.0f, 0.0f,  1.0f, 1.0f, 0.0f,   1.0f, 0.0f, 0.0f}
+        };
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        
+    }
+    void createInstance() {
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+    }
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+        vkDestroyInstance(instance, nullptr);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    BlackBoxEngine app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
